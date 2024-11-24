f9fb5073e7864f5300f423bc0e16c063744c12b6
Peter Aleksander Bizjak <peter.aleksander@bizjak.dev>
2024-11-24 09:36:21 +0100

    Remove unnecessary library name

--- lib/mlkit_document_scanner.dart
+++ lib/mlkit_document_scanner.dart
@@ -1,5 +1,5 @@
 /// Flutter plugin bringing MLKit Document Scanner to Flutter.
-library mlkit_document_scanner;
+library;
 
 export 'package:mlkit_document_scanner/mlkit_document_scanner_enums.dart';
 export 'package:mlkit_document_scanner/mlkit_document_scanner_interface.dart';
